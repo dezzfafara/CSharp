aeeefaf81900a3ea2274bf971e872b7ff1f0feb3
dezzfafara <dezzfafara@gmail.com>
2017-05-27 11:43:18 +0200

    Adding Task 2

---
+++ LAB2/LAB2/ArraysMerging.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2
+{
+    class ArraysMerging
+    {
+        private const int sizeM = 10, sizeN = 5;
+        int[] arrayA = new int[sizeM];
+        int[] arrayB = new int[sizeN];
+        int[] arrayC = new int[sizeM + sizeN];
+        int[] tempArray = new int[sizeM + sizeN];
+        private void SetArrays()
+        {
+            Random random = new Random();
+            for (int i = 0; i < arrayA.Length; i++)
+                arrayA[i] = random.Next(1, 10);
+            for (int i = 0; i < arrayB.Length; i++)
+                arrayB[i] = random.Next(1, 10);
+        }
+        private void PushIntoArrayC()
+        {
+            int count = 0;
+            for (int i = 0; i < arrayA.Length; i++)
+            {
+                for (int j = 0; j < arrayB.Length; j++)
+                {
+                    if (arrayA[i] == arrayB[j])
+                    {
+                        arrayC[count] = arrayA[i];
+                        count++;
+                    }
+                }
+            }
+        }
+        private void PushIntoTempArray()
+        {
+            int count = 0;
+            Array.Copy(arrayC, tempArray, tempArray.Length);
+            Array.Sort(tempArray);
+            for (int i = 1; i < tempArray.Length; i++)
+            {
+                if (tempArray[i - 1] != tempArray[i])
+                {
+                    arrayC[count] = tempArray[i];
+                    count++;
+                }
+            }
+            Array.Resize(ref arrayC, count);
+        }
+        public void GetNewArray()
+        {
+            SetArrays();
+            PushIntoArrayC();
+            PushIntoTempArray();
+            Console.WriteLine("Массив А:");
+            for (int i = 0; i < arrayA.Length; i++)
+                Console.Write(arrayA[i] + " ");
+            Console.WriteLine();
+            Console.WriteLine("Массив В:");
+            for (int i = 0; i < arrayB.Length; i++)
+                Console.Write(arrayB[i] + " ");
+            Console.WriteLine();
+            Console.WriteLine("Массив С:");
+            for (int i = 0; i < arrayC.Length; i++)
+                Console.Write(arrayC[i] + " ");
+            Console.WriteLine();
+
+        }
+    }
+}
--- LAB2/LAB2/Program.cs
+++ LAB2/LAB2/Program.cs
@@ -4,24 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson2
+namespace LAB2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var Array = new Arrays();
-            Array.SetArrayB();
-            Array.ShowArrayB();
-            Array.SetArrayA();
-            Array.ShowArrayA();
-            Console.WriteLine();
-            Console.WriteLine("Максимальный элемент равен: " + Array.GetMaxArrayB());
-            Console.WriteLine("Минимальный элемент равен: " + Array.GetMinArrayB());
-            Console.WriteLine("Сумма всех элементов равна: " + Array.Summ());
-            Console.WriteLine("Произведение всех элементов равно: " + Array.Multiply());
-            Console.WriteLine("Сумма четных элеметов массива А: " + Array.EvenSummA());
-            Console.WriteLine("Сумма элементов нечетных столбцов массива В: " + Array.OddColumnSummB());
+            //var Array = new Arrays();
+            //Array.SetArrayB();
+            //Array.ShowArrayB();
+            //Array.SetArrayA();
+            //Array.ShowArrayA();
+            //Console.WriteLine();
+            //Console.WriteLine("Максимальный элемент равен: " + Array.GetMaxArrayB());
+            //Console.WriteLine("Минимальный элемент равен: " + Array.GetMinArrayB());
+            //Console.WriteLine("Сумма всех элементов равна: " + Array.Summ());
+            //Console.WriteLine("Произведение всех элементов равно: " + Array.Multiply());
+            //Console.WriteLine("Сумма четных элеметов массива А: " + Array.EvenSummA());
+            //Console.WriteLine("Сумма элементов нечетных столбцов массива В: " + Array.OddColumnSummB());
+            var MergedArray = new ArraysMerging();
+            MergedArray.GetNewArray();
 
         }
     }
