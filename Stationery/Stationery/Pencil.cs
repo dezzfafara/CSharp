b10d1f545294c185f14cdcfa274822b6cc6645ae
dezzfafara <dezzfafara@gmail.com>
2017-05-31 18:01:40 +0200

    Adding Stationery Class

---
+++ Stationery/Stationery/Pencil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stationery
+{
+    class Pencil
+    {
+        double _price;
+        string _type, _color, _producer;
+        bool _hasEraser;
+        static int _totalQuantity;
+        string inputData;
+        public double GetPrice()
+        {
+            return _price;
+        }
+        public string GetType()
+        {
+            return _type;
+        }
+        public string GetColor()
+        {
+            return _color;
+        }
+        public string GetProducer()
+        {
+            return _producer;
+        }
+        public bool GetHasEraser()
+        {
+            return _hasEraser;
+        }
+        public void SetColor()
+        {
+            Console.WriteLine("Введите цвет:");
+            _color = Console.ReadLine();
+        }
+    }
+}
---
+++ Stationery/Stationery/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stationery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pencil[] pencil = new Pencil[5];
+
+        }
+    }
+}
