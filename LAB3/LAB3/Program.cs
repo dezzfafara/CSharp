477662559617c7348cabbf13f6575148c51eb12f
dezzfafara <dezzfafara@gmail.com>
2017-06-12 14:32:15 +0200

    Task 1 added

---
+++ LAB3/LAB3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputData;
+            var arr = new RangeOfArray(-4, 3);
+            Console.WriteLine("Длина массива: " + arr.Length);
+            for (int i = arr.GetMin(); i <= arr.GetMax(); i++)
+            {
+                Console.WriteLine("Введите " + i + "-й элемент:");
+                InputData = Console.ReadLine();
+                arr[i] = new Number { number = double.Parse(InputData) };
+            }
+            Console.WriteLine();
+            for (int i = arr.GetMin(); i <= arr.GetMax(); i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+        }
+    }
+}
---
+++ LAB3/LAB3/RangeOfArray.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB3
+{
+    class RangeOfArray
+    {
+        private int _Min, _Max;
+        private string _InputData;
+        Number[] MyArray;
+        public RangeOfArray(int _min, int _max)
+        {
+            _Min = _min;
+            _Max = _max;
+            MyArray = new Number[_Max - _Min + 1];
+        }
+        public Number this[int index]
+        {
+            get
+            {
+                if (index >= _Min && index <= _Max)
+                {
+                    return MyArray[index - _Min];
+                }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                MyArray[index - _Min] = value;
+            }
+        }
+        public int Length
+        {
+            get
+            {
+                return MyArray.Length;
+            }
+        }
+
+        public int GetMin()
+        {
+            return _Min;
+        }
+        public int GetMax()
+        {
+            return _Max;
+        }
+    }
+}
